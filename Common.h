c7b95da79aef4324c501b9e3059b15c7eee2e008
itamarmaouda101 <itamarmaouda101@gmail.com>
2021-02-23 16:16:53 +0200

    "add user mode client"

--- Common.h
+++ Common.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <ntddk.h>
 #define DeviceType 0x800
-#define IOCTL_DRIVER_SET CTL_CODE(DeviceType, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
+#define IOCTL_DRIVER_SET_PRIORITY CTL_CODE(DeviceType, 0x800, METHOD_NEITHER, FILE_ANY_ACCESS)
 struct ThreadData
 {
 	ULONG ThreadId;
